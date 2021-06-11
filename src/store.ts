fb132b0111806b565d262a5eef1fbe47b4d11883
Sergey <ya.sergey137@gmail.com>
2021-06-11 17:15:19 +0300

    Local storage

--- src/store.ts
+++ src/store.ts
@@ -3,10 +3,18 @@ import { useDispatch } from "react-redux";
 
 import notesReducer from "./reducers/notes";
 
+const noteStateJson = localStorage.getItem("notesState");
+const preloadedState = noteStateJson ? JSON.parse(noteStateJson) : {};
+
 export const store = configureStore({
   reducer: {
     notesReducer,
   },
+  preloadedState,
+});
+
+store.subscribe(() => {
+  localStorage.setItem("notesState", JSON.stringify(store.getState()));
 });
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
